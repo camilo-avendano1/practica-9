dc00270363fc74672f415730d5f0460ce54393c9
Camilo avendaño <camilo.avendano1@udea.edu.co>
2023-05-19 14:58:34 -0500

    fix error stackvaluable

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
 const terminalArr = ['+', '-', '*', '/', '(', ')', 'num', 'id', '$']
 const nonTerminalArr = ['E', 'E\'', 'T', 'T\'', 'F']
-const stack = []
+
+const rules = [
+    //   +   -    *   /  (    )  num id  $
+        ['', '', '', '', 'TE\'', '', 'TE\'', 'TE\'', ''],
+        ['+TE\'', '-TE\'', '', '', '', '@', '', '', '@'],
+        ['', '', '', '', 'FT\'', '', 'FT\'', 'FT\'', ''],
+        ['@', '@', '*FT\'', '/FT\'', '', '@', '', '', '@'],
+        ['', '', '', '', '(E)', '', 'num', 'id', '']
+    ]
+
+    const entry = "id+num*id$" //verification string
+
+    stackableValue = rules[getNonTerminalIndex(lastStackToken)][getTerminalIndex(entryTokenArr)]
+    console.log(stackableValue);
 
 
-
-
-
-function popStack(str){
-    let stack = str.split('');
-    let variable = stack.pop();
-    if(variable == "'" ) variable = stack.pop() + variable;
-    return variable
-}
 
 function addToStack(stack, val){
-    let token;
-    for(let  i = val.length; i == 0; i--){
-        token = ''
-        if(val[i] == "'") token = "'"
+    console.log('Hola')
+    let valArr = val.split('') 
+    token = ''
+    for(let  i = valArr.length - 1; i >= 0; i--){
+        console.log(i)
+        if(valArr[i] === "'") token = "'"
         else{
-            token = val[i] + token
+            console.log('Pasa por acá con token: ' + token)
+            token = valArr[i] + token
             stack.push(token)
+            token = ''
         }
-    stack.push(val);
+    }
 }
-
-const value = "TE'"
-const stack = []
-addToStack(value, stack);
-console.log(stack);
 
 function compare(val1,val2) {
     return val1 === val2
@@ -42,32 +45,33 @@ function getNonTerminalIndex(nonTerminalVarible){
     return nonTerminalArr.indexOf(nonTerminalVarible)
 }
 
-//matrix of rules for the grammar
-const rules = [
-//   +   -    *   /  (    )  num id  $
-    ['', '', '', '', 'TE\'', '', 'TE\'', 'TE\'', ''],
-    ['+TE\'', '-TE\'', '', '', '', '@', '', '', '@'],
-    ['', '', '', '', 'FT\'', '', 'FT\'', 'FT\'', ''],
-    ['@', '@', '*FT\'', '/FT\'', '', '@', '', '', '@'],
-    ['', '', '', '', '(E)', '', 'num', 'id', '']
-]
 
-const entry = "id+num*id$" //verification string
+
 const entryTokensArr = ['id', '+' , 'num' , '*' , 'id', '$']
 function evaluateEntry(entryTokenArr, rules){
     const initialValue = 'E'
     const stack = ['$']
     let entryToken;
+    let lastStackToken;
+    let stackableValue;
     stack.push(initialValue)
-    while(stack[stack.length - 1] !== $){ 
+    while(stack[stack.length - 1] !== '$'){ 
         entryToken = entryTokenArr[0];
-        if(compare(entryToken , stack[stack.length-1])){
+        lastStackToken = stack[stack.length-1]
+        if(compare(entryToken , lastStackToken)){
             entryTokenArr.shift()
+            stack.pop()
         } else{
-            stackableValue = rules[getNonTerminalIndex(entryTokenArr)]
-            addToStack(stackableValue)
+            console
+            stackableValue = rules[getNonTerminalIndex(lastStackToken)][getTerminalIndex(entryTokenArr)]
+            console.log(stackableValue);
+            stack.pop()
+            addToStack(stack,stackableValue)
         }
-        
     }
+    if (entryTokensArr[entryTokensArray.lenght-1] === '$') return 'Es válida'
+    return 'No es válida'
 }
-}
+
+//matrix of rules for the grammar
+
